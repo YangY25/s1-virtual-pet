c18aa6b583988d32570bee6f2b2ab047db35552d
Your Name <you@example.com>
2023-10-06 12:07:18 -0700

    yelq

--- cam-v1/RunnerMain.java
+++ cam-v1/RunnerMain.java
@@ -1,6 +1,39 @@
+import javax.swing.*;
+
+
 public class RunnerMain {
-    public static void main(String[] args){
+    public RunnerMain(){
         VirtualPet v = new VirtualPet();
+        v.exercise();
+        takeABeat(1000);
         v.feed();
+        String ans = getAnswer("How are you?");
+        if(ans.equals("Good")){
+            v.respondPostitively();
+        }
     }
+
+    public String getAnswer(String q){
+        String s = (String)JOptionPane.showInputDialog(
+                    new JFrame(),
+                    "Your Message",
+                    "Your Title",
+                    JOptionPane.PLAIN_MESSAGE
+                );
+                if(s.equals("Yes"));
+
+    }
+
+    public void takeABeat(int milliseconds){
+        try {
+            Thread.sleep(milliseconds); //milliseconds
+        } catch(Exception e){
+
+        }
+    }
+
+    public static void main(String[] args){
+        new RunnerMain(); 
+    }   
+
 }
