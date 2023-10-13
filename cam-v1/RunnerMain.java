56a3060fea8a1afcc9f124d35a2fb87c8d330e97
Your Name <you@example.com>
2023-10-13 11:32:10 -0700

    aaaaaaaaa

--- cam-v1/RunnerMain.java
+++ cam-v1/RunnerMain.java
@@ -6,8 +6,6 @@ public class RunnerMain {
         VirtualPet v = new VirtualPet();
         v.exercise();
         takeABeat(1000);
-        v.feed();
-        takeABeat(1000);
         v.play();
         takeABeat(1000);
         String ans = getAnswer("Do you want to know what I did today?");
@@ -17,8 +15,20 @@ public class RunnerMain {
             v.notToday();
         }
         takeABeat(1000);
-        v.hungry();
         String ans1 = getAnswer("Can you feed me?");
+        if(ans1.equals("Yes")){
+            String an = getAnswer("How many time you want to feed?");
+            for (int i = 0; i < Integer.valueOf(an); i++) {
+                v.feed();
+            }
+        }else{
+            v.notToday();
+        }
+        takeABeat(1000);
+        v.checkHealth();
+        String ans2 = getAnswer("Is it night right now?");
+        v.time(ans2);
+        v.sleep();
     }
 
     public String getAnswer(String q){
